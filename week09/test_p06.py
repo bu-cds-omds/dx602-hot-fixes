6d7f3b1bb78aff71e3b7d5219cdcdc4bec9f9f92
Jeffrey Considine <jconsidi@gmail.com>
2024-10-25 13:03:41 -0400

    + week 9 p6

---
+++ week09/test_p06.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import inspect
+import homework
+
+import test_utils
+
+
+class P6TestCase(test_utils.TestCase):
+    def test_p6(self):
+        self.assertTrue(hasattr(homework, "p6"), "Variable p6 is missing.")
+
+        p6 = homework.p6
+        self.assertFalse(isinstance(p6, type(Ellipsis)), "Variable p6 was not set.")
+
+        self.assertValues(p6, expected_projections=[144.0384696884941])
+
+
+############################################################
+# startup handling #########################################
+############################################################
+
+if __name__ == "__main__":
+    unittest.main()
